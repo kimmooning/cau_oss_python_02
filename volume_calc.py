0ff6bf92b4d642795730b42dcb97a3219e1a281f
kimmooning <136059841+kimmooning@users.noreply.github.com>
2024-03-19 16:30:37 +0900

    Update volume_calc.py
    총 길이에 따른 택배요금 계산 코드

--- volume_calc.py
+++ volume_calc.py
@@ -1,4 +1,13 @@
-outcome_num = 1
+total_length = 0
 for i in ["가로: ","세로: ","높이: "]:
-        outcome_num *= float(input(i))
-print(outcome_num)
+        total_length += float(input(i))
+if total_length <= 80:
+    total_price = 5000
+elif total_length <= 100:
+    total_price = 8000
+elif total_length <= 120:
+    total_price = 10000
+elif total_length <= 160:
+    total_price = 13000
+print(total_price)
+
